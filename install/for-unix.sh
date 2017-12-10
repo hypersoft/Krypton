86abcac7accc7bafcbbab2580493b128e1996118
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-12-09 22:03:42 -0600

    Automated Install Support

--- install/for-unix.sh
+++ install/for-unix.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo
-
 RLWRAP=`type -p rlwrap`;
 BASH=`type -p bash`;
 
@@ -15,39 +13,52 @@ BASH=`type -p bash`;
 
 KRYPTON=`pwd`
 
-JAR_PATH=${0%*install/for-unix.sh}jar/krypton.jar
+JAR_PATH="${0%*install/for-unix.sh}jar/*.jar"
 
 INSTALL_LIB_PATH=/usr/lib
 INSTALL_BIN_PATH=/bin
 
-read -e -p "This script will install (library) krypton.jar to: " -i "$INSTALL_LIB_PATH" INSTALL_LIB_PATH;
+[[ "$1" == '--automatic' ]] || {
+echo
+read -e -p "This script will install krypton (library) directory to: " -i "$INSTALL_LIB_PATH" INSTALL_LIB_PATH;
+}
 
 test -w "$INSTALL_LIB_PATH" || {
-    echo "You do not have the permissions needed to install krypton.jar to: $INSTALL_LIB_PATH";
+    echo "You do not have the permissions needed to install krypton libraries to: $INSTALL_LIB_PATH";
     exit 1;
 }
 
+INSTALL_LIB_PATH="$INSTALL_LIB_PATH/krypton";
+if [[ -e "$INSTALL_LIB_PATH" ]]; then
+    rm -rf "$INSTALL_LIB_PATH"/*;
+else
+    mkdir -v "$INSTALL_LIB_PATH";
+fi
+
+[[ "$1" == '--automatic' ]] ||
 read -e -p "This script will install (program) krypton to: " -i "$INSTALL_BIN_PATH" INSTALL_BIN_PATH;
 
 test -w "$INSTALL_BIN_PATH" || {
-    echo "You do not have the permissions needed to install krypton to: $INSTALL_BIN_PATH";
+    echo "You do not have the permissions needed to install the krypton program to: $INSTALL_BIN_PATH";
     exit 1;
 }
 
 echo
 
-# install the library
-cp -v "$JAR_PATH" "$INSTALL_LIB_PATH";
+echo "installing required libraries...";
+cp -v $JAR_PATH "$INSTALL_LIB_PATH" || exit $?;
 
-# install the launcher
 echo $'\n'"generating $INSTALL_BIN_PATH/krypton launcher..."
+{
 cat <<EOF > "$INSTALL_BIN_PATH/krypton"
 #!${BASH}
 
 ${RLWRAP}java -jar ${INSTALL_LIB_PATH}/krypton.jar "\$@";
 
 EOF
+} || exit $?;
 
-chmod +x "$INSTALL_BIN_PATH/krypton";
+echo
+chmod -v +x "$INSTALL_BIN_PATH/krypton" || exit $?;
 
 echo $'\n'"Thank you for choosing Krypton by Hypersoft-Systems: U.-S.-A."$'\n';
